0773e7ba583ce7522d6ee773b51ed4e54a275d8e
DGclasher <73327721+DGclasher@users.noreply.github.com>
2023-08-21 10:27:56 +0530

    updaed

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ app.use("/url", require("./routes/url"));
 app.use("/", require("./routes/redirect"));
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static(path.join(__dirname, "client")));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
 }
 
